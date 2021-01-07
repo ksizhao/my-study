e2c42cdd7b08358197f08611d8b76fae34957d7f
sarcae <8312407+sarcae@user.noreply.gitee.com>
2021-01-07 13:53:04 +0800

    feat:课程表

--- javanio-write/src/main/java/com/example/javaniowrite/solution/Solution.java
+++ javanio-write/src/main/java/com/example/javaniowrite/solution/Solution.java
@@ -178,11 +178,6 @@ public class Solution {
 
     }
 
-    public static void main(String[] args) {
-        String str = "hello world";
-        System.out.println(reverseStr(str));
-    }
-
 
     /**
      * 两数相加
@@ -3183,12 +3178,13 @@ public class Solution {
      * 我们可以将二维网格看成一个无向图，竖直或水平相邻的 11之间有边相连。
      * 为了求出岛屿的数量，我们可以扫描整个二维网格。如果一个位置为 1，则以其为起始节点开始进行深度优先搜索。在深度优先搜索的过程中，每个搜索到的 1都会被重新标记为 0。
      * 最终岛屿的数量就是我们进行深度优先搜索的次数
+     *
      * @param grid
      * @return
      */
     public int numIslands(char[][] grid) {
 
-        if (grid == null || grid.length == 0) {
+        if (grid == null || grid[0].length == 0) {
             return 0;
         }
         int r = grid.length;
@@ -3218,80 +3214,99 @@ public class Solution {
         dfs(grid, row, column + 1);
     }
 
+    static class WordsFrequency {
+        private Map<String, Integer> integerMap = new HashMap<>();
 
-//    public static void main(String[] args) {
-//        //  String s = "a";
-//        int[] nums = {1, 2, 3, 1};
-////        int[] arrays = topKFrequent(nums, 2);
-////        String s = "asbnb";
-////        long start = System.currentTimeMillis();
-////        System.out.println(findPeakElement(nums));
-////        long end = System.currentTimeMillis();
-////        System.out.println("耗时" + (end - start));
-//        //  System.out.println(longestPalindrome(s));
-//        // System.out.println(stringList.toString());
-//        //System.out.println(titleToNumber("B"));
-////        Arrays.sort(nums);
-////        System.out.println(nums);
-//        //  String s = "anagram";
-//        // String t = "nagaram";
-//        // bubbleSort(nums);
-////        quickSort(nums, 0, nums.length - 1);
-////        System.out.println(Arrays.toString(nums));
-////        String str = "0003320F3298182||01|051440202065|TX09|61973669|882005251708423112|4200000533202005252079547943|1.00|1|SUCCESS|2020-05-25 17:08:47|20200525||0.00|1026100000020";
-////        String[] strings = StringUtils.split(str, "|");
-////        System.out.println(Arrays.toString(strings));
-//        // System.out.println(isAnagram(s, t));
-//        //System.out.println(rob(nums));
-//
-//        // System.out.println(partition(s));
-////        //int result = searchInsert(nums, 2);
-////        //List<Integer> result = getRow(3);
-////        int[] result = exchange(nums);
-////        for (int i = 0; i < result.length; i++) {
-////            System.out.println(result[i]);
-////        }
-////        int n = 100;
-////        int result = sumNums(n);
-////        System.out.println(result);
-//        //System.out.println(singleNumber(nums));
-////        List<List<Integer>> integerList = permute(nums);
-////        System.out.println(integerList);
-//        //int result = lengthOfLastWord1(s);
-////        List<List<String>> listList = groupAnagrams(nums);
-////        System.out.println(listList);
-////        long start = System.currentTimeMillis();
-////        int result = countPrimes(count);
-////        long end = System.currentTimeMillis();
-////        System.out.println("耗时:" + (end - start));
-////        System.out.println(result);
-////        String string = "23484";
-////        int [] ints={0,4,5,7,0,8};
-////        //List<String> str = letterCombinations(string);
-////        //System.out.println("结果" + str);
-////        moveZeroes(ints);
-//        // Date date=getYesterday();
-////        String date = "20190802";
-////        SimpleDateFormat sb = new SimpleDateFormat("yyyyMMdd");
-////        try {
-////            Date date1 = sb.parse(date);
-////            System.out.println(date1);
-////        } catch (ParseException e) {
-////            e.printStackTrace();
-////        }
-////        System.out.println("启动计数线程");
-////        System.out.println("计数值：" + latch.getCount());
-////        getThread().start();
-////        while (latch.getCount() != 0) {
-////            latch.countDown();
-////            try {
-////                Thread.sleep(1000);
-////            } catch (InterruptedException e) {
-////                e.printStackTrace();
-////            }
-////            System.out.println("计数值：" + latch.getCount());
-////        }
-//    }
+        public WordsFrequency(String[] book) {
+            for (String str : book) {
+                integerMap.put(str, integerMap.getOrDefault(str, 0) + 1);
+            }
+        }
+
+        public int get(String word) {
+            return integerMap.getOrDefault(word, 0);
+        }
+    }
+
+    /**
+     * 加油站
+     *
+     * @param gas
+     * @param cost
+     * @return
+     */
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        // 加油站数量
+        int n = gas.length;
+        int i = 0;
+        while (i < n) {
+            int count = 0;
+            int j = (count + i) % n;
+            int sumGas = 0, sumCost = 0;
+            while (count < n) {
+                sumGas += gas[j];
+                sumCost += cost[j];
+                if (sumCost > sumGas) {
+                    break;
+                }
+                count++;
+            }
+            if (count == n) {
+                return i;
+            } else {
+                i = i + count + 1;
+            }
+        }
+        return -1;
+    }
+
+    List<List<Integer>> edges;
+    int[] indeg;
+
+    /**
+     * 课程表
+     * @param numCourses
+     * @param prerequisites
+     * @return
+     */
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        edges = new ArrayList<List<Integer>>();
+        for (int i = 0; i < numCourses; ++i) {
+            edges.add(new ArrayList<Integer>());
+        }
+        indeg = new int[numCourses];
+        for (int[] info : prerequisites) {
+            edges.get(info[1]).add(info[0]);
+            ++indeg[info[0]];
+        }
+
+        Queue<Integer> queue = new LinkedList<Integer>();
+        for (int i = 0; i < numCourses; ++i) {
+            if (indeg[i] == 0) {
+                queue.offer(i);
+            }
+        }
+
+        int visited = 0;
+        while (!queue.isEmpty()) {
+            ++visited;
+            int u = queue.poll();
+            for (int v: edges.get(u)) {
+                --indeg[v];
+                if (indeg[v] == 0) {
+                    queue.offer(v);
+                }
+            }
+        }
+
+        return visited == numCourses;
+    }
+
+    public static void main(String[] args) {
+        String[] strings = {"i", "have", "an", "apple", "he", "have", "a", "pen"};
+        WordsFrequency wordsFrequency = new WordsFrequency(strings);
+        System.out.println(wordsFrequency.get("have"));
+    }
 
 
 }
